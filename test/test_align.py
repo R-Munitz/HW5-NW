aba33496f8e8453d197f151beeb7e11bf6717be5
Rochel Munitz <rochelmunitz@gmail.com>
2025-02-12 16:54:08 -0800

    added placeholder unit tests, .yml, .toml files

--- test/test_align.py
+++ test/test_align.py
@@ -14,6 +14,38 @@ def test_nw_alignment():
     """
     seq1, _ = read_fasta("./data/test_seq1.fa")
     seq2, _ = read_fasta("./data/test_seq2.fa")
+
+    nw = NeedlemanWunsch(sub_matrix_file="substitution_matrices/BLOSUM62.mat", gap_open=-10, gap_extend=-1)
+    score, align1, align2 = nw.align(seq1, seq2)
+
+    #assert that alignment matrix is correct
+    expected_alignment_mat = [[  0., -10., -11., -12.], #double check this is correct?
+                                [-10.,   5.,  -5.,  -6.],
+                                [-11.,  -5.,   4.,   2.],
+                                [-12.,  -6.,  10.,   9.]]
+                                
+    alignment_mat = nw.alignment_matrix
+    assert np.array_equal(alignment_mat, expected_alignment_mat)
+
+    #note, expected matrices are just placeholders, need to check if they are correct
+    gapA_mat = nw.gapA_matrix
+    gapB_mat = nw.gapB_matrix
+
+    #assert that gapA matrix is correct
+    expected_gapA_mat = [[  0., -10., -20., -30.],
+                        [-10., -10., -11., -21.],
+                        [-20.,  -5., -15., -16.],
+                        [-30.,  -6.,  -5., -15.]]
+    assert np.array_equal(gapA_mat, expected_gapA_mat)
+
+    #assert that gapB matrix is correct
+    expected_gapB_mat = [[  0., -10., -20., -30.],
+                        [-10.,  -5., -15., -16.],
+                        [-20., -10.,  -5., -15.],
+                        [-30., -11.,  -6.,  -5.]]
+    assert np.array_equal(gapB_mat, expected_gapB_mat)
+
+
     pass
     
 
@@ -27,6 +59,27 @@ def test_nw_backtrace():
     """
     seq3, _ = read_fasta("./data/test_seq3.fa")
     seq4, _ = read_fasta("./data/test_seq4.fa")
+
+    nw = NeedlemanWunsch(sub_matrix_file="substitution_matrices/BLOSUM62.mat", gap_open=-10, gap_extend=-1)
+
+    score, align3, align4 = nw.align(seq3, seq4)
+
+    #assert that backtrace matrix is correct
+    backtrace_mat = nw.backtrace_matrix
+    #placeholder expected backtrace matrix
+    expected_backtrace_mat = [[0, 0, 0, 0],
+                                [0, 1, 1, 1],
+                                [0, 1, 1, 1],
+                                [0, 1, 1, 1]]
+    assert np.array_equal(backtrace_mat, expected_backtrace_mat)
+
+    #assert alignment score is correct
+    assert score == 17
+
+    #assert alignment seqs are correct
+    assert align3 == "MAVHQLIRRP"
+    assert align4 == "M---QLIRHP"
+
     pass
 
 
